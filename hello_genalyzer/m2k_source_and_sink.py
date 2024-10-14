22b45572e6a7d33ba77e5d9c390355026ddf2cb7
Ioan Dragomir <ioan.dragomir@analog.com>
2024-10-14 16:22:42 +0300

    hello_genalyzer/m2k_source_and_sink.py: Fix sampling frequency mismatch, add comments
    Signed-off-by: Ioan Dragomir <ioan.dragomir@analog.com>

--- hello_genalyzer/m2k_source_and_sink.py
+++ hello_genalyzer/m2k_source_and_sink.py
@@ -1,5 +1,5 @@
 import libm2k
-import genalyzer_advanced as gn
+import genalyzer.advanced as gn
 import numpy as np
 import matplotlib.pyplot as pl
 from matplotlib.patches import Rectangle as MPRect
@@ -10,7 +10,6 @@ ctx=libm2k.m2kOpen()
 if ctx is None:
 	print("Connection Error: No ADALM2000 device available/connected to your PC.")
 	exit(1)
-
 
 ain=ctx.getAnalogIn()
 aout=ctx.getAnalogOut()
@@ -23,13 +22,19 @@ aout.reset()
 ctx.calibrateADC()
 ctx.calibrateDAC()
 
+# Input and output will be done at different sampling frequencies
+# Need to make sure we're not mixing these up later!
+fs_in  = 100000
+fs_out = 750000
+
+# Initialize m2k ain & aout channels
 ain.enableChannel(0,True)
 ain.enableChannel(1,True)
-ain.setSampleRate(100000)
+ain.setSampleRate(fs_in)
 ain.setRange(0,-10,10)
 
-aout.setSampleRate(0, 750000)
-aout.setSampleRate(1, 750000)
+aout.setSampleRate(0, fs_out)
+aout.setSampleRate(1, fs_out)
 aout.enableChannel(0, True)
 aout.enableChannel(1, True)
 
@@ -38,16 +43,13 @@ navg = 2  # No. of fft averages
 nfft = 1024 * 256  # FFT order
 # Get number of points
 npts = navg * nfft
-fs = 750000.0  # Sampling frequency in Hz for m2k DAC
 
 phase = 0.0  # Tone phase
 td = 0.0
 tj = 0.0
-qres = 16  # Quantizer for m2k ADC
-qnoise_dbfs = -140.0  # Quantizer noise
 code_fmt = gn.CodeFormat.TWOS_COMPLEMENT  # ADC codes format
 rfft_scale = gn.RfftScale.NATIVE  # FFT scale
-window = gn.Window.NO_WINDOW  # FFT window
+window = gn.Window.BLACKMAN_HARRIS  # FFT window
 
 fsr = 2.0  # Full-scale range
 fund_freq = 10000.0  # Hz
@@ -63,7 +65,7 @@ harm_dbfs = [-3.0, -23.0, -20.0, -20.0]
 # harmonically related and add them to the total noise.
 noise_freqs = [15000.0, 25000.0, 35000.0, 45000.0]
 # Replace -200.0 with greater values to add noise tones
-noise_dbfs = [-200.0, -200.0, -200.0, -200.0]
+noise_dbfs = [-40, -50, -70, -60]
 
 # Calculate absolute amplitudes from dBfs.
 harm_ampl = []
@@ -73,55 +75,46 @@ noise_ampl = []
 for x in range(len(noise_dbfs)):
     noise_ampl.append((fsr / 2) * 10 ** (noise_dbfs[x] / 20))
 
-ssb_fund = 4  # Single side bin fundamental
-ssb_rest = 5
+# Single side bin "width" around peaks
+# If these values are too low, spectral leakage near signal peaks will be mistakenly detected as noise
+ssb_fund = 1000 # Single side bins for fundamental and harmonics
+ssb_rest = 1000 # Single side bins for DC and WorstOther
+
 # If we are not windowing then choose the closest coherent bin for fundamental
 if gn.Window.NO_WINDOW == window:
-    fund_freq = gn.coherent(nfft, fs, fund_freq)
+    fund_freq = gn.coherent(nfft, fs_out, fund_freq)
     ssb_fund = 0
     ssb_rest = 0
 
-# Now build up the signal from the fundamental, harmonics, and noise tones
+# Build up the signal from the fundamental, harmonics, and noise tones
 awf = np.zeros(npts)
 
 for harmonic in range(len(harm_dbfs)):
     freq = fund_freq * (harmonic + 1)
-    print("Frequency: ", freq)
+    print(f"Frequency: {freq} ({harm_dbfs[harmonic]} dBfs)")
 
-    awf += gn.cos(npts, fs, harm_ampl[harmonic], freq, phase, td, tj)
+    awf += gn.cos(npts, fs_out, harm_ampl[harmonic], freq, phase, td, tj)
 
 for tone in range(len(noise_freqs)):
     freq = noise_freqs[tone]
     if gn.Window.NO_WINDOW == window:
-        freq = gn.coherent(nfft, fs, noise_freqs[tone])
-    print("Noise Frequency: ", freq)
-    awf += gn.cos(npts, fs, noise_ampl[tone], freq, phase, td, tj)
+        freq = gn.coherent(nfft, fs_out, noise_freqs[tone])
+    print(f"Noise Frequency: {freq} ({noise_dbfs[tone]} dBfs)")
+    awf += gn.cos(npts, fs_out, noise_ampl[tone], freq, phase, td, tj)
 
-
-
-# Plot analog waveform
-pl.figure(1)
-pl.plot(awf[:10000])
-# Get quantizer noise in Volts
-
-#print(np.array(qwf).shape)
 # push awf to M2K DAC
 aout.setCyclic(True)
 aout.push([awf, awf])
 # get data on M2K ADC
 data_in = ain.getSamples(npts)[0]
-pl.figure(2)
-pl.plot(data_in[:1000])
-qnoise = 10 ** (qnoise_dbfs / 20)
 
-
-# Quantize waveform
-#qwf = gn.quantize(np.array(data_in), fsr, qres, qnoise, code_fmt)
+# Remove DC component
+data_in = data_in - np.average(data_in)
 
 # Compute FFT
 fft_cplx = gn.rfft(np.array(data_in),navg, nfft, window, code_fmt, rfft_scale)
 # Compute frequency axis
-freq_axis = gn.freq_axis(nfft, gn.FreqAxisType.REAL, fs)
+freq_axis = gn.freq_axis(nfft, gn.FreqAxisType.REAL, fs_in)
 # Compute FFT in db
 fft_db = gn.db(fft_cplx)
 
@@ -136,7 +129,7 @@ gn.fa_ssb(key, gn.FaSsb.DEFAULT, ssb_rest)
 gn.fa_ssb(key, gn.FaSsb.DC, -1)
 gn.fa_ssb(key, gn.FaSsb.SIGNAL, -1)
 gn.fa_ssb(key, gn.FaSsb.WO, -1)
-gn.fa_fsample(key, fs)
+gn.fa_fsample(key, fs_in)
 print(gn.fa_preview(key, False))
 
 # Fourier analysis results
@@ -160,13 +153,13 @@ for k in ['snr', 'fsnr']:
 print("{:20s}{:20.6f}".format("thd", thd))
 
 # Plot FFT
-pl.figure(2)
+pl.figure(1)
 fftax = pl.subplot2grid((1, 1), (0, 0), rowspan=2, colspan=2)
 pl.title("FFT")
 pl.plot(freq_axis, fft_db)
 pl.grid(True)
 #pl.xlim(freq_axis[0], 20)
-#pl.ylim(-160.0, 20.0)
+pl.ylim(-160.0, 20.0)
 annots = gn.fa_annotations(fft_results)
 
 for x, y, label in annots["labels"]:
