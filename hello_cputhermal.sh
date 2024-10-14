f12414ff38fe30f496987a7d27fc7d52bf500ab5
Cristina Suteu <cristina.suteu@analog.com>
2024-10-14 15:33:26 +0300

    hello_cputhermal: add example script add script to read cpu thermal remove initial iio examples
    Signed-off-by: Cristina Suteu <cristina.suteu@analog.com>

---
+++ hello_cputhermal.sh
@@ -0,0 +1,12 @@
+
+URI="local:"
+
+while true
+do
+temp_mc=$(iio_attr -u $URI -c cpu_thermal temp1 input)
+temp_c=$(echo "$temp_mc/1000" | bc)
+temp_f=$(echo "($temp_mc/1000)*1.8 + 32" | bc)
+echo "CPU Temperature is $temp_f degrees Fahrenheit"
+echo "And $temp_c Communist Degrees"
+sleep 5
+done
