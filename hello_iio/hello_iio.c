21e64f1eaa2af26d5905f002019268471385eca2
Cristina Suteu <cristina.suteu@analog.com>
2024-10-16 12:32:49 +0300

    hello_iio: add C iio example
    Signed-off-by: Cristina Suteu <cristina.suteu@analog.com>

---
+++ hello_iio/hello_iio.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iio.h>
+
+#define URI "local:"
+
+int main() {
+
+	struct iio_context *ctx;
+	struct iio_device *dev;
+	struct iio_channel *chn;
+	char value[20];
+	double temp_f; 
+	double temp_c;
+
+	ctx = iio_create_context_from_uri(URI);
+	if(!ctx) {
+		printf("No context\n");
+		return -1;
+	}
+	dev = iio_context_find_device(ctx, "cpu_thermal");
+	if(!dev) {
+		printf("No such device \n");
+		return -1;
+	}
+	chn = iio_device_find_channel(dev, "temp1", false);
+
+	iio_channel_attr_read(chn, "input", value, sizeof(value));
+	
+	temp_c = atoi(value)/ 1000.0;
+	temp_f = temp_c * 1.8 + 32 ;
+	printf("%f Freedom Degrees \n", temp_f);
+	printf("%f Communist Degrees \n", temp_c);
+
+	iio_context_destroy(ctx);
+
+	return 0;
+
+}
