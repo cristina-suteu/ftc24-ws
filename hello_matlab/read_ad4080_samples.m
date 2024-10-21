1c662e405564bc3a2cf3b4cdc6586080b596f252
Ioan Dragomir <ioan.dragomir@analog.com>
2024-10-21 16:32:15 +0300

    hello_matlab: Add instructions
    Signed-off-by: Ioan Dragomir <ioan.dragomir@analog.com>

--- hello_matlab/read_ad4080_samples.m
+++ hello_matlab/read_ad4080_samples.m
@@ -1,12 +1,12 @@
 rx = adi.AD4080.Rx;
-rx.uri = 'ip:172.18.228.187'; % Change this to the IP address on which iiod is listening
-
+rx.uri = 'ip:10.48.65.170:50906'; % Change this to the IP address on
+                                  % which iiod is listening
 rx.SamplesPerFrame = 16384;
 rx.EnabledChannels = [1];
 
 data = rx();
+data = data * rx.Scale / 1e3; % Scale is in mV
 
-figure(1);
 plot(data);
 
 % Delete the system object
